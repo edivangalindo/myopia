0a3cb2c62d3a5c0ef3338cac8c15e371f8e6c4c8
Edivan de Barros Galindo <edivan.galindo@gmail.com>
2016-04-02 11:51:40 -0300

    Criação da estrutura do projeto. Criado um projeto Domain e Common.

---
+++ myopia.Common/Validation/EmailAssertionConcern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace myopia.Common.Validation
+{
+    public class EmailAssertionConcern
+    {
+        public static void AssertIsValid(string email)
+        {
+            if (!Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))
+                throw new Exception("Email Inválido");
+        }
+    }
+}
---
+++ myopia.Domain/Models/User.cs
@@ -0,0 +1,69 @@
+﻿using myopia.Common.Resources;
+using myopia.Common.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myopia.Domain
+{
+    public class User
+    {
+        #region Construtor
+
+        protected User() { }
+
+        public User(string name, string email)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+            this.Password = Password;
+        }
+
+        #endregion
+
+        #region Propriedades
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        #endregion
+
+        #region Metodos
+
+        public void SetPassword(string password, string confirmPassword)
+        {
+            AssertionConcern.AssertArgumentNotNull(password, Errors.InvalidUserPassword);
+            AssertionConcern.AssertArgumentNotNull(confirmPassword, Errors.InvalidUserPassword);
+            AssertionConcern.AssertArgumentLength(password, 6, 20, Errors.InvalidUserPassword);
+            AssertionConcern.AssertArgumentEquals(password, confirmPassword, Errors.PasswordDoesNotMatch);
+
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+        }
+
+        public string ResetPassword()
+        {
+            string password = Guid.NewGuid().ToString().Substring(0, 8);
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+
+            return password;
+        }
+
+        public void ChangeName(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Validate()
+        {
+            AssertionConcern.AssertArgumentLength(this.Name, 3, 250, Errors.InvalidUserEmail);
+            EmailAssertionConcern.AssertIsValid(this.Email);
+            PasswordAssertionConcern.AssertIsValid(this.Password);
+        }
+
+        #endregion
+    }
+}
